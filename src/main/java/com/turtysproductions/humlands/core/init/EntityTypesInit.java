7a57ef081d5111fcf022cc1062891b72fdf3e231
MrShawshank <61604648+MrShawshank@users.noreply.github.com>
2020-05-02 09:25:45 +0200

    Update EntityTypesInit.java

--- src/main/java/com/turtysproductions/humlands/core/init/EntityTypesInit.java
+++ src/main/java/com/turtysproductions/humlands/core/init/EntityTypesInit.java
@@ -2,6 +2,8 @@ package com.turtysproductions.humlands.core.init;
 
 import com.turtysproductions.humlands.HumlandsMod;
 import com.turtysproductions.humlands.common.entities.Humadillo;
+import com.turtysproductions.umlands.common.entities.Humahog;
+import com.turtysproductions.umlands.common.entities.RubberBallEntity;
 
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
@@ -12,9 +14,21 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class EntityTypesInit {
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = new DeferredRegister<>(ForgeRegistries.ENTITIES,
-			HumlandsMod.MOD_ID);
+	HumlandsMod.MOD_ID);
 
-	public static final RegistryObject<EntityType<Humadillo>> HUMADILLO = ENTITY_TYPES.register("humadillo",
-			() -> EntityType.Builder.<Humadillo>create(Humadillo::new, EntityClassification.CREATURE).size(0.9f, 1.3f)
-					.build(new ResourceLocation(HumlandsMod.MOD_ID, "humadillo").toString()));
+	public static final RegistryObject<EntityType<Humadillo>> HUMADILLO = ENTITY_TYPES
+	.register("humadillo", () -> EntityType.Builder.<Humadillo>create(Humadillo::new, EntityClassification.CREATURE)
+	.size(0.9f, 1.3f)
+        .build(new ResourceLocation(HumlandsMod.MOD_ID, "humadillo").toString()));
+	
+	public static final RegistryObject<EntityType<Humahog>> HUMAHOG = ENTITY_TYPES
+        .register("humahog", () -> EntityType.Builder.<Humahog>create(Humahog::new, EntityClassification.MONSTER)
+        .size(0.9f, 1.3f)
+        .build(new ResourceLocation(HumlandsMod.MOD_ID, "humahog").toString()));
+        
+        public static final RegistryObject<EntityType<RubberBallEntity>> RUBBER_BALL_ENTITY = ENTITY_TYPES
+        .register("rubber_ball_entity", () -> EntityType.Builder.<RubberBallEntity>create(RubberBallEntity::new, EntityClassification.MISC)
+        .size(0.9f, 1.3f)
+        .build(new ResourceLocation(HumlandsMod.MOD_ID, "rubber_ball_entity").toString()));
+	
 }
